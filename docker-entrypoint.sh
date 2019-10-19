28343e7ad45a5b35e95d841522b33bb38affd4c5
Trent Gardner <trent.gardner@gmail.com>
2019-10-19 18:59:23 +1030

    Add proxy variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+[[ -z "$REGISTRY_PROXY_FQDN" ]] && $REGISTRY_PROXY_FQDN=$DOCKER_REGISTRY_HOST
+
 envsubst '${DOCKER_REGISTRY_HOST} ${DOCKER_REGISTRY_PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+echo "{\"host\": \"$REGISTRY_PROXY_FQDN\", \"port\": $DOCKER_REGISTRY_PORT}" > /app/registry-host.json
 
 exec "$@"
